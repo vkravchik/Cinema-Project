2e784414a29e73d57f0e39ccdebfb322e55208b4
vkravc <vkravchik@gmail.com>
2020-03-03 15:25:48 +0200

    some improvements

--- server/index.js
+++ server/index.js
@@ -4,6 +4,7 @@ const mongoose = require('mongoose');
 const bluebird = require('bluebird');
 const bodyParser = require('body-parser');
 const multer = require('multer');
+const path = require('path');
 
 require('dotenv').config();
 
@@ -28,6 +29,8 @@ app.use(cors());
 app.use(express.json());
 app.use(multerMid.single('file'));
 app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.static(path.join(__dirname, '../dist/AngularTesting')));
+
 
 app.get('/api/ping', (req, res) => {
   res.send('pong');
@@ -36,6 +39,10 @@ app.get('/api/ping', (req, res) => {
 app.use('/api/users', require('./routes/userRoutes'));
 app.use('/api/films', require('./routes/filmRoutes'));
 app.use('/api/genres', require('./routes/genreRoutes'));
+
+app.get('*', function (req, res) {
+  res.sendFile(path.join(__dirname, '../dist/AngularTesting', 'index.html'));
+});
 
 mongoose.connect(`mongodb://${dbUrl}:${dbPort}/${dbName}`, {
   useNewUrlParser: true,
